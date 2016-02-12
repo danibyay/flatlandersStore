903bec0a33b4cca74063b15a24397f1febbe45b7
Daniela Becerra <madajiji@gmail.com>
2016-02-11 23:28:03 -0600

    add 2 gem images

--- app.js
+++ app.js
@@ -8,14 +8,26 @@
   var gems = [
     {
       name: 'Dodecahedron',
-      price: 2.95,
+      price: 2,
       description: ' This is a very cool gem, buy it right now',
+      images: [
+          {
+            full: 'x.png',
+            thumb:'y.png'
+          }
+      ],
       canPurchase: true,
       soldOut: false
     },
     {
       name: 'Pentagonal Gem',
       price: 5.95,
+      images: [
+          {
+            full: 'w.png',
+            thumb:'z.png'
+          }
+      ],
       description: ' This is a very cool gem, buy it right now',
       canPurchase: true,
       soldOut: true
